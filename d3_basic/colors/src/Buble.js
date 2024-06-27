f63da1460955a51d615cb10699cc762d1f9d2099
motos@ubits.co <millerandresoc@gmail.com>
2024-06-27 16:42:47 -0500

    d3

--- d3_basic/colors/src/Buble.js
+++ d3_basic/colors/src/Buble.js
@@ -1,26 +1,52 @@
-import React, { useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
+import { csv, arc } from "d3";
 
-const width = 600;
-const height = 600;
-const initialPosition = { x: width / 2, y: height / 2 };
+
+const cvsGit = "https://gist.githubusercontent.com/curran/b236990081a24761f7000567094914e0/raw/cssNamedColors.csv"
+
 
 export const Boble = () => {
 
-    const [mousePosition, setMousePostion] = useState(initialPosition)
-    const mouseEventHandle = useCallback((event) => {
-        const { clientX, clientY } = event
-        setMousePostion({ x: clientX, y: clientY })
+    const [data, setData] = useState(null);
 
-    }, [setMousePostion])
+    useEffect(() => {
+        // transform csv with d3.csv
+        csv(cvsGit).then(data => {
+            console.log(data)
+            setData(data)
+        });
+
+    }, [])
+
+    //create instance for arc d3
+    const arcD3 = arc()
     return (
-        <div>
-            <h1 id='hola'>Hola Como van?</h1>
-            <svg width={width} height={height} onMouseMove={mouseEventHandle}>
-                <circle cx={mousePosition.x} cy={mousePosition.y} stroke='black' stroke-width="1" r={10} />
-            </svg>
-        </div>
+        <svg height={1000} width={1000}>
+            <g transform={`translate(${500},500)`}>
+            {data ?
+                
+                data.map((color,i) => (
+                    
+                    <path d={
+                        arcD3({
+                            innerRadius:0,
+                            outerRadius:500,
+                            startAngle: i / data.length * 2 * Math.PI,
+                            endAngle: (data.length * 2 * Math.PI) / (i +1)
+                        })
+                        
+                    } 
+                    
+                    fill={color["RGB hex value"]} 
+                    
+                    s={data.length}
+                    />
+                    
+                ))
 
-
+                : "loading..."}
+                </g>
+        </svg>
     );
 }
 
