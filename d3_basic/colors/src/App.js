7441290ece319498077fbda5a38b2361347ada36
motos@ubits.co <millerandresoc@gmail.com>
2024-06-28 20:49:38 -0500

    d3 graph barras

--- d3_basic/colors/src/App.js
+++ d3_basic/colors/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Buble from './Buble'
+import Band from './Band';
 
 function App() {
   return (
     <div className="App">
-      <Buble />
+      
+      <Band />
     </div>
   );
 }
