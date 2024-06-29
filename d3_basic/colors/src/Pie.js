81c98ce803eae56b0fe36318e36f196c40625e34
motos@ubits.co <millerandresoc@gmail.com>
2024-06-28 20:51:28 -0500

    graph barras

---
+++ d3_basic/colors/src/Pie.js
@@ -0,0 +1,14 @@
+import { svg } from "d3";
+import React from "react";
+
+export const Pie = () => {
+
+    return (
+        <svg>
+            
+        </svg>
+    );
+} 
+
+
+export default Pie;
